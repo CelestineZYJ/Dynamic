04a376df3430aec6ae80ddb62da865326223039c
YujiZhang <864409320@qq.com>
2024-04-06 22:15:54 -0500

    Update read_weibo.py

--- read_weibo.py
+++ read_weibo.py
@@ -2,6 +2,7 @@ import csv
 import ast
 import re
 from tqdm import tqdm
+import jieba
 import csv
 csv.field_size_limit(100000000)  # Set a larger limit (e.g., 100 MB)
 
@@ -28,8 +29,10 @@ def extract_chinese_text(string):
     
     # Join the matches into a single string
     chinese_text = ''.join(chinese_matches)
-    
-    return chinese_text
+    if len(jieba.lcut(chinese_text)) < 10:
+        return False
+    else:
+        return chinese_text
 
 def extract_hashtags(string):
     # Define a regular expression pattern to match hashtags
@@ -40,33 +43,17 @@ def extract_hashtags(string):
     
     return hashtags
 
-def process_user_history_weibo(weibostr):
+def process_user_history_weibo(uid, weibostr):
     new_weibo_list = []
     user_weibo_list = ast.literal_eval(weibostr)
     for each_weibo in user_weibo_list:
         weibo_string = each_weibo[0]
         hashtag_list = extract_hashtags(weibo_string)
-        weibo_time = each_weibo[1]
-        if hashtag_list:
-            chi_weibo_str = extract_chinese_text(weibo_string)
-            new_weibo_list.append([chi_weibo_str, hashtag_list, weibo_time])
+        weibo_time = each_weibo[1].split(' ')[0]
+        chi_weibo_str = extract_chinese_text(weibo_string)
+        if hashtag_list and chi_weibo_str:
+            new_weibo_list.append({'timestamp':weibo_time, 'user':uid, 'text':chi_weibo_str, 'tag_list':hashtag_list})
     return new_weibo_list
-
-# def read_csv_file_by_user(file_path):
-#     data = []
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         csv_reader = csv.reader(file)
-#         # Skip the header row
-#         next(csv_reader)
-#         for row in csv_reader:
-#             # Assuming the CSV file has three columns: id, weibo, paper_time
-#             if len(row) == 6:
-#                 _, uid, nested_list_str, date, some_value, another_value = row
-#             if len(row) == 4:
-#                 _, uid, nested_list_str, date = row
-#             user_weibo_list = process_user_history_weibo(nested_list_str)
-#             data.append({'uid': uid, 'user_weibo':user_weibo_list,'ana_timestamp': date})
-#     return data
 
 def read_csv_file(file_path):
     data = []
@@ -74,11 +61,11 @@ def read_csv_file(file_path):
         csv_reader = csv.reader(file)
         # Skip the header row
         next(csv_reader)
-        for row in csv_reader:
+        for row in tqdm(csv_reader):
             # Assuming the CSV file has three columns: id, weibo, paper_time
-            uid, user_all_weibos, timestamp = row[1], row[2], row[3]
-            user_weibo_list = process_user_history_weibo(user_all_weibos)
-            data.append({'uid': uid, 'user_weibo':user_weibo_list,'timestamp': timestamp})
+            uid, user_all_weibos = row[1], row[2]
+            this_user_weibo_list = process_user_history_weibo(uid, user_all_weibos)
+            data.extend(this_user_weibo_list)
     return data
 
 def analyze_users(file_path):
@@ -93,6 +80,41 @@ def analyze_users(file_path):
             ulist.append(uid)
     return ulist
 
+def extract_tag_dict(time_list):
+    tag_dict = {}
+    for each_weibo in tqdm(time_list):
+        timestamp, uid, weibo_text, u_tag_list = each_weibo[0], each_weibo[1], each_weibo[2], each_weibo[3]
+        for tag in u_tag_list:
+            if tag not in tag_dict:
+                tag_dict[tag]={'context_num':0, 'users':{}}
+            if uid not in tag_dict[tag]['users']:
+                tag_dict[tag]['users'][uid]=[]
+            tag_dict[tag]['users'][uid].append({'text':weibo_text, 'timestamp':timestamp})
+            tag_dict[tag]['context_num']+=1
+    for tag in tag_dict:
+        if len(tag_dict[tag]['users'].keys())>100:
+            del tag_dict[tag]
+            print('delete a tag used by more than 100 users')
+    print('totally '+str(len(tag_dict.keys()))+' tags in this subset')
+    return tag_dict
+
+def extract_user_dict(time_list):
+    user_dict = {}
+    for each_weibo in tqdm(time_list):
+        timestamp, uid, weibo_text, u_tag_list = each_weibo[0], each_weibo[1], each_weibo[2], each_weibo[3]
+        if uid not in user_dict:
+            user_dict[uid] = {'weibo_num':0, 'tags':{}}
+        for tag in u_tag_list:
+            if tag not in user_dict[uid]['tags']:
+                user_dict[uid]['tags'][tag]=[]
+            user_dict[uid]['tags'][tag].append({'text':weibo_text, 'timestamp':timestamp})
+            user_dict[uid]['weibo_num']+=1
+    for user in user_dict:
+        if len(user_dict[user]['weibo_num'])>100:
+            del user_dict[user]
+            print('delete a user posting more than 100 weibos')
+    print('totally '+str(len(user_dict.keys()))+' users in this subset')
+    return user_dict
 
 train_file_path = './weibo_data/train.csv'  # Replace 'sample.csv' with your actual file path
 test1_file_path = './weibo_data/test1.csv'
@@ -105,6 +127,35 @@ val_file_path = './weibo_data/dev.csv'
 # val_ulists = analyze_users(val_file_path)
 # count_shared_elements(train_ulist, test1_ulist, test2_ulist, val_ulists) # 0 shared user in four subsets
 
+all_data = []
 train_data = read_csv_file(train_file_path)
-for row in train_data[:20]:
-    print(row)
+all_data.extend(train_data)
+test1_data = read_csv_file(test1_file_path)
+all_data.extend(test1_data)
+test2_data = read_csv_file(test2_file_path)
+all_data.extend(test2_data)
+val_data = read_csv_file(val_file_path)
+all_data.extend(val_data)
+print('There are totally '+str(len(all_data))+' weibos with length longer than 10 in the original data')
+time_sorted_list = sorted(all_data, key=lambda x: x['timestamp'])
+
+time_sorted_list = time_sorted_list[20000:] # only retain data in a reasonable time span, the list[0]['time']=2009, the list[200000]['time']=2019.12, the list[-1]['time']=2021.01
+
+past_train = time_sorted_list[:int(len(time_sorted_list)*0.5)]
+future_train = time_sorted_list[int(len(time_sorted_list)*0.5):int(len(time_sorted_list)*0.8)]
+future_test = time_sorted_list[int(len(time_sorted_list)*0.8):]
+
+past_train_tag_context = extract_tag_dict(past_train)
+future_train_tag_context = extract_tag_dict(future_train)
+future_test_tag_context = extract_tag_dict(future_test)
+
+shared_hastags = list(set(past_train_tag_context.keys())&set(future_train_tag_context.keys())&set(future_test_tag_context.keys()))
+print('there are totally '+str(len(shared_hastags))+' hashtags shared by three subsets')
+
+past_train_user_weibo = extract_user_dict(past_train)
+future_train_user_weibo = extract_user_dict(future_train)
+future_test_user_weibo = extract_user_dict(future_test)
+
+shared_users = list(set(past_train_user_weibo.keys())&set(future_train_user_weibo.keys())&set(future_test_user_weibo.keys()))
+print('there are totally '+str(len(shared_users))+' users shared by three subsets')
+
